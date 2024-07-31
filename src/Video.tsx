53be1e402e585eeca0aa4f27729fe4e3e96fa10e
Darren Taft <darren.taft@bt.com>
2024-07-31 08:34:04 +0100

    Updated naming convention.  Prevent error callback also being called on success.

--- src/Video.tsx
+++ src/Video.tsx
@@ -512,8 +512,8 @@ const Video = forwardRef<VideoRef, ReactVideoProps>(
       [onControlsVisibilityChange],
     );
 
-    const sourceDrm = source?.drm || drm;
-    const usingExternalGetLicense = sourceDrm?.getLicense instanceof Function;
+    const selectedDrm = source?.drm || drm;
+    const usingExternalGetLicense = selectedDrm?.getLicense instanceof Function;
 
     const onGetLicense = useCallback(
       async (event: NativeSyntheticEvent<OnGetLicenseData>) => {
@@ -525,21 +525,24 @@ const Video = forwardRef<VideoRef, ReactVideoProps>(
         if (data?.spcBase64) {
           try {
             // Handles both scenarios, getLicenseOverride being a promise and not.
-            const license = await sourceDrm.getLicense(
+            const license = await selectedDrm.getLicense(
               data.spcBase64,
               data.contentId,
               data.licenseUrl,
               data.loadedLicenseUrl,
             );
-            result =
-              typeof license === 'string' ? license : 'Empty license result';
-              if (nativeRef.current && typeof license === 'string') {
+            if (typeof license === 'string') {
+              if (nativeRef.current) {
                 NativeVideoManager.setLicenseResultCmd(
                   getReactTag(nativeRef),
                   result,
                   data.loadedLicenseUrl,
                 );
               }
+              return;
+            } else {
+              result = 'Empty license result';
+            } 
           } catch {
             result = 'fetch error';
           }
@@ -554,7 +557,7 @@ const Video = forwardRef<VideoRef, ReactVideoProps>(
           );
         }
       },
-      [sourceDrm, usingExternalGetLicense],
+      [selectedDrm, usingExternalGetLicense],
     );
 
     useImperativeHandle(
